ffc6019bb46c6ad5e5f7faee82060ded7fcc0b42
Marouf <maroufibrahimi7264@gmail.com>
2023-05-07 19:31:17 -0700

    link every navbar content

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "react-router-dom"
 import Logo from "../images/logo.png"
 
 const Navbar = () => {
@@ -8,7 +9,32 @@ const Navbar = () => {
         <div className="logo">
           <img src={Logo} alt="logo" />
         </div>
-        <div className="links">links</div>
+
+        <div className="links">
+          <Link className="link" to="/?cat=art">
+            <h6>ART</h6>
+          </Link>
+          <Link className="link" to="/?cat=technology">
+            <h6>TECHNOLOGY</h6>
+          </Link>
+          <Link className="link" to="/?cat=cinema">
+            <h6>CINEMA</h6>
+          </Link>
+          <Link className="link" to="/?cat=design">
+            <h6>DESIGN</h6>
+          </Link>
+          <Link className="link" to="/?cat=food">
+            <h6>FOOD</h6>
+          </Link>
+
+          <span>Famous</span>
+          <span>Logout</span>
+          <span className="write">
+            <Link className="link" to="/write">
+              Write
+            </Link>
+          </span>
+        </div>
       </div>
     </div>
   )
