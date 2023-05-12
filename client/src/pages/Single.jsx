2fea57b998702dfa08558fa411e129ec4259ecbb
Marouf <maroufibrahimi7264@gmail.com>
2023-05-12 06:49:27 -0700

    header img for single

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from "react"
+import img3 from "../images/img3.jpg"
+import user1 from "../images/user1.jpg"
 
 const Single = () => {
   return (
-    <div>Single</div>
+    <div className="single">
+      <div className="content">
+        <img src={img3} alt="content img" />
+        <div className="user">
+          <img src={user1} alt="user img" />
+        </div>
+      </div>
+    </div>
   )
 }
 
