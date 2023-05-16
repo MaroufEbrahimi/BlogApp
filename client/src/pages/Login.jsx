084e8b597e8e91c7803bc1fad851048c24b2f64a
Marouf <maroufibrahimi7264@gmail.com>
2023-05-16 11:16:02 -0700

    added login page detailt

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from "react"
+import { Link } from "react-router-dom"
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className="login">
+      <h1>Login</h1>
+      <form>
+        <input type="text" placeholder="username" />
+        <input type="password" placeholder="password" />
+        <button>Login</button>
+        <p>This is an error!</p>
+        <span>
+          Don't you have an account? <Link to="/register">Register</Link>
+        </span>
+      </form>
+    </div>
   )
 }
 
