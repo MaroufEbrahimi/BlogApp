2fcb7f80fbd6834ba3bc054712911de2df83fe4f
Marouf <maroufibrahimi7264@gmail.com>
2023-05-17 20:53:45 -0700

    added register page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from "react"
+import { Link } from "react-router-dom"
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className="register login">
+      <h1>Register</h1>
+      <form>
+        <input type="text" placeholder="username" required />
+        <input type="password" placeholder="password" required />
+        <input type="email" placeholder="email" required />
+        <button>Register</button>
+        <p>This is an error!</p>
+        <span>
+          Do you have an account? <Link to="/login">Login</Link>
+        </span>
+      </form>
+    </div>
   )
 }
 
