e006e2f5e2c920cc7e011b4377688d5e43adf654
Marouf <maroufibrahimi7264@gmail.com>
2023-05-17 21:21:10 -0700

    added an text editor for change message

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from "react"
+import ReactQuill from "react-quill"
+import "react-quill/dist/quill.snow.css"
 
 const Write = () => {
+  const [value, setValue] = useState("")
+
   return (
-    <div>Write</div>
+    <div className="add">
+      <div className="content">
+        <input type="text" placeholder="Title" />
+        <div className="editorContainer">
+          <ReactQuill
+            className="editor"
+            theme="snow"
+            value={value}
+            onChange={setValue}
+          />
+        </div>
+      </div>
+
+      <div className="menu">m</div>
+    </div>
   )
 }
 
