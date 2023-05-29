3c4ff696c9d0d58e021343d4b23cb06cfeabbff6
Marouf <maroufibrahimi7264@gmail.com>
2023-05-28 20:54:02 -0700

    hash the password and create user

--- api/controller/auth.js
+++ api/controller/auth.js
@@ -1,4 +1,5 @@
 import { db } from "../db.js"
+import bcrypt from "bcryptjs"
 
 export const register = (req, res) => {
   // check existing user
@@ -7,5 +8,18 @@ export const register = (req, res) => {
   db.query(dbQuery, [req.body.email, req.body.username], (err, data) => {
     if (err) return res.json(err)
     if (data.length) return res.status(409).json("User already exists!")
+
+    // Hash the password and create user
+    const salt = bcrypt.genSaltSync(10)
+    const hash = bcrypt.hashSync(req.body.password, salt)
+
+    const insertQuery =
+      "INSERT INTO users(`username`, `email`, `password`) VALUES(?)"
+    const values = [req.body.username, req.body.email, hash]
+
+    db.query(insertQuery, [values], (err, data) => {
+      if (err) return res.json(err)
+      return res.status(200).json("User has been created!")
+    })
   })
 }
